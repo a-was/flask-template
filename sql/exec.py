e08a6f8774f3659e982fcc7d498e07d12096c097
Adam <44528455+a-was@users.noreply.github.com>
2020-09-02 13:00:06 +0200

    try/except

--- sql/exec.py
+++ sql/exec.py
@@ -8,6 +8,11 @@ sql_files = [f for f in os.listdir('.') if f.endswith('.sql')]
 for sql_file in sql_files:
     with open(sql_file) as f:
         for command in f.read().split(';'):
-            db.execute(command)
-            db.commit()
+            try:
+                db.execute(command)
+            except sqlite3.IntegrityError:
+                print('WARNING - This command cannot be executed due to IntegrityError - {}'.format(command.strip()))
+            except sqlite3.OperationalError:
+                print('ERROR - This command raised OperationalError - {}'.format(command.strip()))
+db.commit()
 db.close()
